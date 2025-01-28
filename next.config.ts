869f263b961a40a1a63c17ab21ad24a8553b8c19
AndrewGrovez <andrew@golcentres.co.uk>
2025-01-28 11:30:46 -0500

    config: disable ESLint and configure image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images-eu.ssl-images-amazon.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.amazon.co.uk'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
